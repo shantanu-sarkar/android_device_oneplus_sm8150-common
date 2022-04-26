95beb6fd0c939c758784b91a5a182bb0911bffde
dataoutputstream <mb150996@gmail.com>
2022-04-25 20:56:35 -0400

    sm8150-common: Build required audio modules
    - Without these modules, logcat gets spammed with:
            AudioSystem-JNI: AudioSystem::listAudioPorts error -19

--- common.mk
+++ common.mk
@@ -153,6 +153,8 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.0 \
     android.hardware.bluetooth.audio@2.0-impl \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
+    audio.primary.default \
+    audio.primary.msmnile \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
