d4deaee952462f906184a98eb4963acbb907a539
Master <mhmdeveloperofficial@gmail.com>
2023-10-12 17:41:32 +0800

    Disable sparse for ext images
    * Sparse images cause kPostInstallMountError
    
    Co-authored-by: yairpatch <yair.zarfati@gmail.com>
    Change-Id: Ifdb2fc87d88c9c2139b3e73b6ccad36f09888b75
    Signed-off-by: ralph950412 <ralph950412@gmail.com>
    Signed-off-by: Simão Gomes Viana <devel@superboring.dev>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -129,6 +129,9 @@ TARGET_USES_ION := true
 BOARD_USES_QCOM_HARDWARE := true
 TARGET_BOARD_PLATFORM := msmnile
 
+# Disable sparse for ext images
+TARGET_USERIMAGES_SPARSE_EXT_DISABLED := true
+
 # Metadata
 BOARD_USES_METADATA_PARTITION := true
 
